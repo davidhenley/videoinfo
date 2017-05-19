05c2c113f3f10e1f4516fedec655f780decc8555
David Henley <henleydr@gmail.com>
2017-05-19 11:43:48 -0500

    ipcRenderer and ipcMain

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 app.on('ready', () => {
   const main = new BrowserWindow({});
 
   main.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, { format }) => {
+    console.log(`Your file is ${format.duration} seconds!`);
+  });
 });
